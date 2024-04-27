dfd3d5095982f70046fd8669636f168f7d6be538
sundayjava <sundaydavidudoekong@gmail.com>
2024-04-27 14:52:52 +0100

    pottt

--- src/app/_component/productDetailTab/PurchasedTogether.tsx
+++ src/app/_component/productDetailTab/PurchasedTogether.tsx
@@ -8,9 +8,9 @@ const PurchasedTogether = () => {
         <h1 className="font-bold mb-8">Our customers also purchased:</h1>
       </div>
       <div className="md:grid-cols-3 sm:grid-cols-2 grid-cols-1 grid">
-        {limitedEditionData.map((item) => (
+        {/* {limitedEditionData.map((item) => (
           <ItemsCard item={item} key={item.id} />
-        ))}
+        ))} */}
       </div>
     </div>
   );
